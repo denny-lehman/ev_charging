8145241069327efd7da0e12943a17a2cbe126e6e
denny-lehman <den@berkeley.edu>
2024-01-24 22:38:35 -0700

    created env_setup.sh

---
+++ env_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ENV_NAME="ev"
+echo $ENV_NAME
+
+# conda update conda
+echo removing environment $ENV_NAME
+conda remove -n $ENV_NAME --all
+
+
+echo 'Create build environment'
+# # See https://github.com/pytorch/vision/issues/7296 for ffmpeg
+conda create \
+  --name ev \
+  --quiet --yes \
+  python=3.11 pip
+
+conda activate ev
+
+pip install pandas numpy matplotlib seaborn
+
+pip freeze>requirements.txt
