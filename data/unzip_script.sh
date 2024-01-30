d46a112f298d9d52c57c072d18bb70f5bae01acb
denny-lehman <den@berkeley.edu>
2024-01-29 19:56:51 -0700

    updated .sh scripts

--- data/unzip_script.sh
+++ data/unzip_script.sh
@@ -8,8 +8,25 @@ cd ..
 
 echo unzipping caltech directory
 cd caltech
+cd California_Garage_01
 gunzip *.gz
 cd ..
+cd California_Garage_02
+gunzip *.gz
+cd ..
+cd LIGO_01/
+gunzip *.gz
+cd ..
+cd S_Wilson_Garage_01
+echo $pwd
+gunzip *.gz
+cd ..
+cd N_Wilson_Garage_01
+gunzip *.gz
+cd ..
+
+cd ..
+
 
 echo unzipping jpl directory
 cd jpl
