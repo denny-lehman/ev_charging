f1386768f4731c2ce5be2dd41b43221efade6c66
Varun Venkatesh <varun2venkatesh@gmail.com>
2024-02-27 12:21:06 -0800

    update to src
    Known bug: creation of X leads to a DateTimeIndex error that needs to be resolved

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
 
+
 def convert_userInputs(x):
     """could do try except block instead"""
     # x is a string of a list of dictionaries, like this
@@ -29,6 +30,7 @@ def userinput_processing(df):
     assert 'userInputs' in list(df.columns)
     return pd.concat([df.drop(columns='userInputs'), df['userInputs'].apply(convert_userInputs)], axis=1)
 
+
 def datetime_processing(df):
     df['connectionTime'] = pd.to_datetime(df['connectionTime'], utc=True, errors='coerce')
     df['connectionTimeHour'] = df['connectionTime'].dt.hour
@@ -41,9 +43,43 @@ def datetime_processing(df):
     df['doneChargingTimeDay'] = df['doneChargingTime'].dt.day
     return df
 
+
 def holiday_processing(df):
     assert 'connectionTime' in df.columns
     cal = calendar()
     holidays = cal.holidays(start=df['connectionTime'].min().date(), end=df['connectionTime'].max().date())
     df['is_holiday'] = df['connectionTime'].isin(holidays)
     return df
+
+
+def create_x(start, end, caiso_fp=None):
+    x = pd.DataFrame(index=pd.date_range(start, end, inclusive='both', freq='h', tz=0),
+                     columns=['dow', 'hour', 'month'])
+    x['dow'] = x.index.dayofweek
+    x['hour'] = x.index.hour
+    x['month'] = x.index.month
+    x['connectionTime'] = x.index
+    x = holiday_processing(x).drop(columns=['connectionTime'])
+    if caiso_fp:
+        caiso = pd.read_csv(caiso_fp)
+        caiso['datetime'] = pd.to_datetime(caiso['date'] + ' ' + caiso['Time'], errors='coerce', utc=True)
+        caiso = caiso.set_index('datetime')
+        caiso_hourly = caiso.groupby(pd.Grouper(freq='1h')).mean()
+        caiso_hourly.index.tz_localize(None)
+        x = x.join(caiso_hourly)
+    return x
+
+
+def create_y(df, start, end, spaceID):
+    tmp = df.copy()
+    tmp = tmp[tmp['spaceID'] == spaceID].sort_index()
+    y = pd.DataFrame(index=pd.date_range(start, end, inclusive='both', freq='h', tz=0),
+                     columns=['is_available', 'sessionID'])
+    y['is_available'] = 1
+    tmp.reset_index(inplace=True)
+    for i in list(tmp.index):
+        start_ = tmp.loc[i, 'connectionTime']
+        end_ = tmp.loc[i, 'disconnectTime']
+        session_ = tmp.loc[i, 'sessionID']
+        y.loc[start_:end_, ['is_available', 'sessionID']] = 0, session_
+    return y  # y is a dataframe with a datetime index and two columns, is_available and sessionID
