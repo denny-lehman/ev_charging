7c5759ad32d02df3fe8a282ed018dec6647f3f58
Varun Venkatesh <varun2venkatesh@gmail.com>
2024-04-02 23:35:17 -0700

    data eng for weather forecast data

--- src/weather.py
+++ src/weather.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+import numpy as np
 
 
 def get_grid_points(latitude, longitude):
@@ -39,9 +40,31 @@ def get_raw_weather_forecast(office, grid_x, grid_y):
 
 
 def create_hourly_forecast_df(json_forecast):
+    # grab each hourly entry in the forecast
     forecast = json_forecast['properties']['periods']
     forecast_df = pd.DataFrame(forecast)
-    forecast_df['probabilityOfPrecipitationPercent'] = forecast_df['probabilityOfPrecipitation'].apply(lambda x: x['value'])
-    forecast_df['dewpoint_degC'] = forecast_df['dewpoint'].apply(lambda x: x['value'])
+
+    # convert startTime to UTC datetime and set as index
+    forecast_df['time'] = pd.to_datetime(forecast_df['startTime'], utc=True)
+    forecast_df = forecast_df.set_index('time')
+
+    # convert temperature to degF and degC
+    forecast_df.rename(columns={'temperature': 'temperature_degF'}, inplace=True)
+    forecast_df['temperature_degC'] = forecast_df['temperature_degF'].apply(lambda x: np.round((x - 32) * 5.0 / 9.0, 2))
+
+    # convert dewpoint temperature to degF and degC
+    forecast_df['dewpoint_degC'] = forecast_df['dewpoint'].apply(lambda x: np.round(x['value'], 2))
+    forecast_df['dewpoint_degF'] = forecast_df['dewpoint_degC'].apply(lambda x: np.round(x * 9.0 / 5.0 + 32, 2))
+
+    # extract value from windSpeed column and convert to int
+    forecast_df['windSpeed_mph'] = forecast_df['windSpeed'].apply(lambda x: int(x.split(" mph")[0]))
+
+    # extract value from json formatted columns
+    forecast_df['probabilityOfPrecipitationPercent'] = forecast_df['probabilityOfPrecipitation'].apply(
+        lambda x: x['value'])
     forecast_df['relativeHumidityPercent'] = forecast_df['relativeHumidity'].apply(lambda x: x['value'])
-    return forecast_df.drop(columns=['number', 'name', 'detailedForecast', 'dewpoint', 'probabilityOfPrecipitation', 'relativeHumidity', 'temperatureTrend'])
+
+    # drop columns that are no longer needed
+    return forecast_df.drop(
+        columns=['startTime', 'endTime', 'windSpeed', 'number', 'name', 'detailedForecast', 'dewpoint',
+                 'probabilityOfPrecipitation', 'relativeHumidity', 'temperatureTrend', 'temperatureUnit'])
