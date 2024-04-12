4a6f14e2b1be6d1d41fe8dc9e1e0f9ca4c5ac605
Elizabeth Willard <57194659+elizabethwillard@users.noreply.github.com>
2024-04-12 20:34:50 +0000

    consolidated for easier arg passing

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 # login to the ecr to get pushed docker image
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 241112547949.dkr.ecr.us-east-1.amazonaws.com
-echo $IMAGE
+
 docker-compose build --build-arg IMAGE_TAG=$IMAGE
 docker-compose up -d 
