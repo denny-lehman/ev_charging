1a0cf5f1ca92f974e8750d283ae4e6494cdbab93
Elizabeth Willard <57194659+elizabethwillard@users.noreply.github.com>
2024-04-12 19:24:14 +0000

    removed parantehs

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 # login to the ecr to get pushed docker image
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 241112547949.dkr.ecr.us-east-1.amazonaws.com
-docker-compose build --build-arg IMAGE_TAG=$(IMAGE)
+docker-compose build --build-arg IMAGE_TAG=$IMAGE
 docker-compose up -d 
