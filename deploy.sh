e1280968619fd25ddead54fa1ef90845faa052ea
Elizabeth Willard <57194659+elizabethwillard@users.noreply.github.com>
2024-04-14 14:31:28 +0000

    readded -d flag to docker compose in deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker system prune --all --force
 
 # login to the ecr to get pushed docker image
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 241112547949.dkr.ecr.us-east-1.amazonaws.com
-docker-compose --env-file .env up 
+docker-compose --env-file .env up -d
