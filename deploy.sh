e008e536fa2602526de09bf3f7ea18a46c000635
Elizabeth Willard <57194659+elizabethwillard@users.noreply.github.com>
2024-04-14 13:35:53 +0000

    call .env in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 # login to the ecr to get pushed docker image
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 241112547949.dkr.ecr.us-east-1.amazonaws.com
 
-docker-compose up 
+docker-compose --env-file .env up 
